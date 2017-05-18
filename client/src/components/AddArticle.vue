e2b102d7ef879adee1050d7881a0c7a8d266d0a2
Oscar Hermawan <email": null>
2017-05-18 17:02:26 +0700

    done

---
+++ client/src/components/AddArticle.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="areasignup">
+      TAMBAH ARTICLE
+  </br> </br>
+    <b-form-input v-model="username" type="text" placeholder="Enter your Username"></b-form-input>
+
+    </br>
+    <b-form-input v-model="password" type="password" placeholder="Enter your Password"></b-form-input>
+    </br>
+    <b-form-input v-model="name" type="text" placeholder="Enter your Name"></b-form-input>
+    </br>
+    <b-button variant="primary" @click="register">
+          Sign Up
+    </b-button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default{
+  data() {
+    return {
+      username:'',
+      password:'',
+      name:''
+    }
+  },
+  methods:{
+    register:function(){
+      console.log('masuk');
+      axios.post('http://localhost:3000/users',{username:this.username,password:this.password, name:this.name})
+      .then((result)=>{
+        window.location.href = "/login"
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+#areasignup{
+  width:50%;
+  margin:0 auto;
+}
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/components/Login'
+import AddArticle from '@/components/AddArticle'
 import Utama from '@/components/Utama'
 import SignUp from '@/components/SignUp'
 
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'Utama',
       component: Utama
+    },
+    {
+      path: '/add-article',
+      name: 'Add Article',
+      component: AddArticle
     },
     {
       path: '/login',
