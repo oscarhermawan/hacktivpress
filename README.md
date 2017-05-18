2c84ce6aae654a3c0da7996d760d88b5adcf6246
Oscar Hermawan <email": null>
2017-05-18 13:26:56 +0700

    readme,model-controller-route-for-user

---
+++ README.md
@@ -0,0 +1,13 @@
+# CRUD USERS AND BLOG WITH mongodb
+ROUTE | HTTP | description
+------| -----|------------
+/users | GET | Get ALL users
+/users/:id | GET | Get single user by ID
+/users | POST | Insert single user
+/users/:id | PUT | Edit User
+/users/:id | DELETE | Delete single user
+/blog | GET | Get ALL blog
+/blog/:id | GET | Get single blog by ID
+/blog | POST | Insert single blog
+/blog/:id | PUT | Edit Blog
+/blog/:id | DELETE | Delete single blog
---
+++ server/routes/users.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const api = require('../controllers/userController')
+var passport = require('passport')
+
+router.get('/', api.getAllUsers)
+router.get('/:id', api.getById)
+router.get('/:id', api.updateUser)
+router.post('/', api.insertUser)
+router.delete('/:id', api.deleteUser)
+
+//login local
+router.post('/signin', passport.authenticate('local', {session:false}), function(req,res){
+  var user = req.user
+  res.send(user)
+})
+
+module.exports = router
